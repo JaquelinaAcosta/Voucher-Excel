34b90d1b2836010db0a6111ab733adc6b4072bc1
JaquelinaAcosta <jaque-acosta@hotmail.com>
2021-07-13 10:52:48 -0300

    repository

--- src/main/java/com/voucherExcel/repository/FiltroExcelRepository.java
+++ src/main/java/com/voucherExcel/repository/FiltroExcelRepository.java
@@ -15,10 +15,17 @@ public interface FiltroExcelRepository extends CrudRepository<Excel, String>{
 
 	Page<Excel> findAll(Pageable pageable);
 	
-	Page<Excel> findByEstado(String estado, Pageable paging);
-	@Query(value = "{'fecha' : {$gte : ?0, $lte: ?1}}")
-	Page<Excel> findByFecha(LocalDate desde, LocalDate hasta, Pageable pageable);
+	Page<Excel> findByEmpresaEmision(String empresaEmision, Pageable pageable);
+	Page<Excel> findByEstadoAndEmpresaEmision(String estado,String empresaEmision,  Pageable paging);
+	@Query(value = "{'fecha' : {$gte : ?0, $lte: ?1}, 'empresaEmision' : ?2}")
+	Page<Excel> findByFechaAndEmpresaEmision(LocalDate desde, LocalDate hasta,String empresaEmision,  Pageable pageable);
+	@Query(value = "{'estado' : ?0, 'fecha' : {$gte : ?1, $lte: ?2}, 'empresaEmision' : ?3}")
+	Page<Excel> findByEstadoAndFechaAndEmpresaEmision(String estado, LocalDate desde, LocalDate hasta, String empresaEmision, Pageable pageable);
+
 	@Query(value = "{'estado' : ?0, 'fecha' : {$gte : ?1, $lte: ?2}}")
 	Page<Excel> findByEstadoAndFecha(String estado, LocalDate desde, LocalDate hasta, Pageable pageable);
+	@Query(value = "{'fecha' : {$gte : ?0, $lte: ?1}}")
+	Page<Excel> findByFecha(LocalDate desde, LocalDate hasta,  Pageable pageable);
+	Page<Excel> findByEstado(String estado, Pageable paging);
 
 }
--- src/main/java/com/voucherExcel/repository/FiltroVoucherRepository.java
+++ src/main/java/com/voucherExcel/repository/FiltroVoucherRepository.java
@@ -16,20 +16,38 @@ public interface FiltroVoucherRepository extends CrudRepository<Voucher, String>
 
 	Page<Voucher> findAll(Pageable pageable);
 	
-	Page<Voucher> findByEstado(String estado, Pageable pageable);
+	//para usuario ROOT
+	Page<Voucher> findByCodigoVoucher(String codigoVoucher, Pageable pageable);
 	Page<Voucher> findByDni(String dni, Pageable pageable);
-	Page<Voucher> findByCodigoVoucher(String codigoVoucher, Pageable pageable);	
-	@Query(value = "{'fechaHasta' : {$gte : ?0, $lte: ?1}}")
-	Page<Voucher> findByFechaHasta(LocalDate desde, LocalDate hasta, Pageable pageable);
+	@Query(value = "{'dni' : ?0, 'estado' : ?1, 'fechaHasta' : {$gte : ?2, $lte: ?3}}")
+	Page<Voucher> findByDniAndEstadoAndFechaHasta(String dni, String estado, LocalDate desde, LocalDate hasta, Pageable pageable);
 	@Query(value = "{'estado' : ?0, 'fechaHasta' : {$gte : ?1, $lte: ?2}}")
 	Page<Voucher> findByEstadoAndFechaHasta(String estado, LocalDate desde, LocalDate hasta, Pageable pageable);
 	@Query(value = "{'dni' : ?0, 'fechaHasta' : {$gte : ?1, $lte: ?2}}")
 	Page<Voucher> findByDniAndFechaHasta(String dni, LocalDate desde, LocalDate hasta, Pageable pageable);
-	@Query(value = "{'dni' : ?0, 'estado' : ?1, 'fechaHasta' : {$gte : ?2, $lte: ?3}}")
-	Page<Voucher> findByDniAndEstadoAndFechaHasta(String dni, String estado, LocalDate desde, LocalDate hasta, Pageable pageable);
+	@Query(value = "{'fechaHasta' : {$gte : ?0, $lte: ?1}}")
+	Page<Voucher> findByFechaHasta(LocalDate desde, LocalDate hasta, Pageable pageable);
+	
+	
+	//Busquedas diferenciando listados por empresa
+	
+	@Query(value = "{'empresaEmision' : ?0}")
+	Page<Voucher> findByEmpresaEmision(String empresaEmision, Pageable pageable);
+	
+	Page<Voucher> findByEstadoAndEmpresaEmision(String estado, String empresaEmision, Pageable pageable);
+	Page<Voucher> findByDniAndEmpresaEmision(String dni, String empresaEmision, Pageable pageable);
+	Page<Voucher> findByCodigoVoucherAndEmpresaEmision(String codigoVoucher, String empresaEmision, Pageable pageable);	
+	@Query(value = "{'fechaHasta' : {$gte : ?0, $lte: ?1}, 'empresaEmision' : ?2}")
+	Page<Voucher> findByFechaHastaAndEmpresaEmision(LocalDate desde, LocalDate hasta, String empresaEmision, Pageable pageable);
+	@Query(value = "{'estado' : ?0, 'fechaHasta' : {$gte : ?1, $lte: ?2}, 'empresaEmision' : ?3}")
+	Page<Voucher> findByEstadoAndFechaHastaAndEmpresaEmision(String estado, LocalDate desde, LocalDate hasta, String empresaEmision, Pageable pageable);
+	@Query(value = "{'dni' : ?0, 'fechaHasta' : {$gte : ?1, $lte: ?2}, 'empresaEmision' : ?3}")
+	Page<Voucher> findByDniAndFechaHastaAndEmpresaEmision(String dni, LocalDate desde, LocalDate hasta, String empresaEmision, Pageable pageable);
+	@Query(value = "{'dni' : ?0, 'estado' : ?1, 'fechaHasta' : {$gte : ?2, $lte: ?3}, 'empresaEmision' : ?4}")
+	Page<Voucher> findByDniAndEstadoAndFechaHastaAndEmpresaEmision(String dni, String estado, LocalDate desde, LocalDate hasta, String empresaEmision, Pageable pageable);
 	
 	//@Query(value = "{ 'estado' : ?0, 'dni' : ?1}")
-	Page<Voucher> findByEstadoAndDni(String estado, String dni, Pageable paging);
+	Page<Voucher> findByEstadoAndDniAndEmpresaEmision(String estado, String dni, String empresaEmision, Pageable paging);
 
 	
 	
